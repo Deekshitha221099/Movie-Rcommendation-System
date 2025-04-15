fa3f9c8faa72eccd9f69a9c3142ae7b6f2161239
Deekshitha221099 <110665150+Deekshitha221099@users.noreply.github.com>
2025-04-15 16:48:52 +0530

    app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def recommend(movie):
     return recommended_movies,recommended_movies_posters
 
 
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movie_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
