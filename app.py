02b9c6c4bb10595e3510ecacee5d5a2953b8f562
Deekshitha221099 <110665150+Deekshitha221099@users.noreply.github.com>
2025-04-15 13:15:43 +0530

    app.py

--- app.py
+++ app.py
@@ -59,6 +59,5 @@ if st.button('Recommend'):
 
 import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+port = int(os.environ.get("PORT", 8080))
+app.run(host='0.0.0.0', port=port)
