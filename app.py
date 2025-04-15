42496b504769a3fb9e808d6f2c6fe32cacc0a6a6
Deekshitha221099 <110665150+Deekshitha221099@users.noreply.github.com>
2025-04-15 12:56:34 +0530

    app.py

--- app.py
+++ app.py
@@ -58,5 +58,7 @@ if st.button('Recommend'):
         st.image(posters[4])
 
 import os
-port = int(os.environ.get("PORT", 8080))
-app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
